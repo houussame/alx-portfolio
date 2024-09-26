b708531e884131d7267f41039df42e046eb72663
houussame <2015.houdou@gmail.com>
2024-09-27 00:47:15 +0100

    Adding List of Skills

---
+++ src/lib/skills.js
@@ -0,0 +1,32 @@
+export const Skills = [
+    {
+        name: "HTML",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+        desc: "The standard markup language for creating web pages and web applications",
+    },
+    {
+        name: "CSS",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+        desc: "Cascading Style Sheets language",
+    },
+    {
+        name: "JavaScript",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+        desc: "A scripting language for the web development",
+    },
+    {
+        name: "React",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        desc: "A JavaScript library for building user interfaces",
+    },
+    {
+        name: "Tailwind",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png",
+        desc: "The utility-first CSS framework",
+    },
+    {
+        name: "Next.js",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg",
+        desc: "The React framework for production",
+    },
+];
